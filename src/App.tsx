77eebd944aff62907ac4f01e04fc5a29da81c940
Semion Shishlov <semionshish@gmail.com>
2025-02-09 02:52:13 +0100

    df

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
         <h1 className="text-center text-4xl font-bold title-main">The first community meme token <br/>created in an online live stream</h1>
       
       <h1 className="text-center text-4xl font-bold abstract">on Abstract</h1>
-      <Carousel />
+      <div className="px-30 w-[80%] mx-auto"><Carousel /></div>
       <div className="card">
         <button className="bg-blue-500 text-2xl font-bold font-size-2xl hover:bg-blue-700 text-xl text-white px-8 m--10 py-8 rounded-full" onClick={() => {}}>
           <a href="https://dexscreener.com/abstract/0x7887b47ec1c904d67891cdef23266380c38a46f1" target="_blank"><h1>BUY $LBD</h1></a>
--- src/Slider.tsx
+++ src/Slider.tsx
@@ -76,7 +76,7 @@ const Carousel = () => {
     })
 
     return (
-        <div className="py-10 relative">
+        <div className="py-10 max-h-[70vh] relative">
             <button onClick={slideToPrevItem} className="absolute top-1/2 -translate-y-1/2 -translate-x-full left-[10%]">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
