aec0aad109667a96ab050d1efef6d6981e8e405b
Semion Shishlov <semionshish@gmail.com>
2025-02-09 01:53:50 +0100

    all

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
 import logo1 from './assets/logo1.png'
-import bg from './assets/bg.jpg'
 import logo from './assets/logo.png'
 import './App.css'
 import video from './assets/vid.mp4'
+import Carousel from './Slider'
+
 
 
 function App() {
   return (
     <>
-      <div>
-      <h1>Lambada presents $LBD </h1>
-        <div>
-          <img src={bg} alt="bg" />
-        </div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={logo1} className="logo react" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={logo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <video src={video} autoPlay loop muted playsInline></video>
+      <div className="container1">
+        <div className="wrapper">
+        <h1 className="text-center text-4xl font-bold title-main">The first community meme token <br/>created in an online live stream</h1>
       
+      <h1 className="text-center text-4xl font-bold abstract">on Abstract</h1>
+      <Carousel />
       <div className="card">
-        <button onClick={() => {}}>
+        <button className="bg-blue-500 hover:bg-blue-700 text-xl text-white px-4 py-4 rounded-md" onClick={() => {}}>
           <a href="https://dexscreener.com/abstract/0x7887b47ec1c904d67891cdef23266380c38a46f1" target="_blank">TRADE NOW</a>
         </button>
       </div>
-      <p className="read-the-docs">
-        DYOR
-      </p>
+      </div>
+      </div>
     </>
   )
 }
