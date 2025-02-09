75e3da054540772807de048dbd0c70c33b3e35c2
Semion Shishlov <semionshish@gmail.com>
2025-02-09 02:37:38 +0100

    sdf

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ function App() {
         <button className="bg-blue-500 text-2xl font-bold font-size-2xl hover:bg-blue-700 text-xl text-white px-8 m--10 py-8 rounded-full" onClick={() => {}}>
           <a href="https://dexscreener.com/abstract/0x7887b47ec1c904d67891cdef23266380c38a46f1" target="_blank"><h1>BUY $LBD</h1></a>
         </button>
+        <p className="text-center py-10 text-l text-white">Our goal is growth. Together. Be a part of history.</p>
       </div>
       </div>
       </div>
